d68e39aab9c8fc74c4ef81863a033c5280ca815e
asharma352csc <56717584+asharma352csc@users.noreply.github.com>
2019-10-26 17:45:51 +0530

    controller
    take required files

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace External_Google_Authentication.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        
+
+        public string Get()
+        {
+            return "Welcome To Web API";
+        }
+        public List<string> Get(int Id)
+        {
+            return new List<string> {
+                "Ashish",
+                "Sharma"
+            };
+        }
+    }
+}
